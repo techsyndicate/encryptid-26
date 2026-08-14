bba260c6f00060a69ca0db4fa1639a2d0bb6ceee
Shyamak-Seth <sethshyamak@gmail.com>
2026-08-15 00:34:27 +0530

    fix: level 0

--- utils/bash.js
+++ utils/bash.js
@@ -12,6 +12,8 @@ const checkCmd = async (cmd, req) => {
         return echoCommand
     } else if (command[0] == 'ksi') {
         return "https://docs.google.com/document/d/10fmVAQpOZw_OeUNW1bn5H4xk7r7onBa_YgTp5xuBwdc/edit?tab=t.0";
+    } else if (command[0] == 'duck') {
+        return "https://encryptid-level0.onrender.com"
     }
     else {
         return `Bash: command "${command[0]}" not found.`
