8a77348cfa743cb38b79bb436b09532509d3bd6b
Ishit Rastogi <ishitrastogi@gmail.com>
2026-08-16 11:45:07 +0530

    fix: browser

--- routers/browserRouter.js
+++ routers/browserRouter.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
             return res.end(`<img src='x' onerror='var mynewwin = window.open("https://pastebin.com/zXmFm60Z", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
         }
         else if(searchUrl == '025114592') {
-            return res.end(`<img src='https://i.ibb.co/SX2cwBcF/Screenshot-2026-08-16-113643.png' onerror='var mynewwin = window.open("https://www.dropbox.com/scl/fi/ffmfqvm38e88wsigequ3z/puzzlepiece4.png?rlkey=u0s53r1l0l23fqhfwrwm4pzcm&st=geusjrey&dl=0", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
+            return res.end(`<img src='https://i.ibb.co/SX2cwBcF/Screenshot-2026-08-16-113643.png' onload='var mynewwin = window.open("https://www.dropbox.com/scl/fi/ffmfqvm38e88wsigequ3z/puzzlepiece4.png?rlkey=u0s53r1l0l23fqhfwrwm4pzcm&st=geusjrey&dl=0", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
         }
         else {
             res.end(`<img src='x' onerror='var mynewwin = window.open("${searchUrl}", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
