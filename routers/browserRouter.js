6f1235cd0444d8018d7941dca79152b777febe87
Ishit Rastogi <130888925+NotIshi28@users.noreply.github.com>
2026-08-16 11:37:59 +0530

    update:broswer image and link

--- routers/browserRouter.js
+++ routers/browserRouter.js
@@ -7,14 +7,14 @@ router.post('/', async (req, res) => {
         var {searchUrl} = req.body
         searchUrl = searchUrl.trim()
         searchUrl = searchUrl.replaceAll('"', '')
-        if (!searchUrl.startsWith('http://') && !searchUrl.startsWith('https://')) {
-            searchUrl = `http://${searchUrl}`
-        }
+        // if (!searchUrl.startsWith('http://') && !searchUrl.startsWith('https://')) {
+        //     searchUrl = `http://${searchUrl}`
+        // }
         if (searchUrl == 'https://www.behance.net/techsyndicate46') {
             return res.end(`<img src='x' onerror='var mynewwin = window.open("https://pastebin.com/zXmFm60Z", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
-        } // idhar daal
+        }
         else if(searchUrl == '025114592') {
-            return res.end(`<img src='x' onerror='var mynewwin = window.open("https://www.dropbox.com/scl/fi/ffmfqvm38e88wsigequ3z/puzzlepiece4.png?rlkey=u0s53r1l0l23fqhfwrwm4pzcm&st=geusjrey&dl=0", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
+            return res.end(`<img src='https://i.ibb.co/SX2cwBcF/Screenshot-2026-08-16-113643.png' onerror='var mynewwin = window.open("https://www.dropbox.com/scl/fi/ffmfqvm38e88wsigequ3z/puzzlepiece4.png?rlkey=u0s53r1l0l23fqhfwrwm4pzcm&st=geusjrey&dl=0", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
         }
         else {
             res.end(`<img src='x' onerror='var mynewwin = window.open("${searchUrl}", \"_blank\"); mynewwin.focus(); window.document.getElementById(\"myimgxyz\").remove()' id='myimgxyz'>`)
